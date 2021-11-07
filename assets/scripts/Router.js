7ed49b8a06952732e5a253faa780c98d0f97c5c9
Raghav <rcherukuru@ucsd.edu>
2021-11-07 08:56:56 -0800

    finished part 1

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] =  pageFunc;
   }
 
   /**
@@ -65,5 +66,23 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+    if(!this[page]) {
+      console.log('err');
+      return;
+    }
+    let hash;
+    if(page == 'home') {
+      hash = '';
+    }
+    else {
+      hash = '#' + page;
+    }
+
+    if(!statePopped && window.location.hash != hash) {
+      history.pushState({page},'', window.location.origin + hash);
+    }
+
+    this[page]();
+
   }
 }
